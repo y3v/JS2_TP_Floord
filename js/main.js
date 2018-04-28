a44cb25fd67129c624921849250e5e281e2d6816
y3v <yevgeni.kantorovich@gmail.com>
2018-04-28 14:51:43 -0400

    some changes

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 ;(function () {
-	
+
 	'use strict';
 
-	// iPad and iPod detection	
+	// iPad and iPod detection
 	var isiPad = function(){
 		return (navigator.platform.indexOf("iPad") != -1);
 	};
 
 	var isiPhone = function(){
 	    return (
-			(navigator.platform.indexOf("iPhone") != -1) || 
+			(navigator.platform.indexOf("iPhone") != -1) ||
 			(navigator.platform.indexOf("iPod") != -1)
 	    );
 	};
@@ -18,14 +18,14 @@
 
 	// Carousel Feature Slide
 	var owlCrouselFeatureSlide = function() {
-		
+
 		var owl = $('.owl-carousel');
 
 		owl.on('initialized.owl.carousel change.owl.carousel',function(elem){
 			var current = elem.item.index;
 			$(elem.target).find(".owl-item").eq(current).find(".to-animate").removeClass('fadeInUp animated');
 			$(elem.target).find(".owl-item").eq(current).find(".to-animate-2").removeClass('fadeInUp animated');
-		
+
 		});
 		owl.on('initialized.owl.carousel changed.owl.carousel',function(elem){
 			setTimeout(function(){
@@ -49,7 +49,7 @@
 		    autoplay: true,
 			autoplayTimeout: 5000,
 			autoplayHoverPause: true,
-		    navText: [	
+		    navText: [
 		      "<i class='icon-arrow-left2 owl-direction'></i>",
 		      "<i class='icon-arrow-right2 owl-direction'></i>"
 	     	]
@@ -65,9 +65,9 @@
 		$('.animate-box').waypoint( function( direction ) {
 
 			if( direction === 'down' && !$(this).hasClass('animated') ) {
-			
+
 				$(this.element).addClass('fadeInUp animated');
-			
+
 			}
 
 		} , { offset: '75%' } );
@@ -81,13 +81,13 @@
 		$('body').on('click', '.js-fh5co-nav-toggle', function(event){
 
 			if ( $('#navbar').is(':visible') ) {
-				$(this).removeClass('active');	
+				$(this).removeClass('active');
 			} else {
-				$(this).addClass('active');	
+				$(this).addClass('active');
 			}
 
 			event.preventDefault();
-			
+
 		});
 
 	};
@@ -129,11 +129,12 @@
 	var navigationSection = function() {
 
 		var $section = $('div[data-section]');
-		
+
+
 		$section.waypoint(function(direction) {
 		  	if (direction === 'down') {
 		    	navActive($(this.element).data('section'));
-		    
+
 		  	}
 		}, {
 		  	offset: '150px'
@@ -160,16 +161,18 @@
 				scrlTop = $(this).scrollTop();
 
 			if ( scrlTop > 500 && scrlTop <= 2000 ) {
+				//if scroll is outside of the home area, fix nav bar to the top of the page -- written by Yev
 				header.addClass('navbar-fixed-top fh5co-animated slideInDown');
 			} else if ( scrlTop <= 500) {
 				if ( header.hasClass('navbar-fixed-top') ) {
+					//if in the home area, defualt nav bar is display -- Written by Yev
 					header.addClass('navbar-fixed-top fh5co-animated slideOutUp');
 					setTimeout(function(){
 						header.removeClass('navbar-fixed-top fh5co-animated slideInDown slideOutUp');
 					}, 100 );
 				}
-			} 
-			
+			}
+
 		});
 	};
 
@@ -180,15 +183,15 @@
 	// About Us
 	var aboutAnimate = function() {
 
-		if ( $('#about-us').length > 0 ) {	
+		if ( $('#about-us').length > 0 ) {
 			$('#about-us .to-animate').each(function( k ) {
-				
+
 				var el = $(this);
-				
+
 				setTimeout ( function () {
 					el.addClass('fadeInUp animated');
 				},  k * 200, 'easeInOutExpo' );
-				
+
 			});
 		}
 
@@ -197,15 +200,15 @@
 
 		if ( $('#about-us').length > 0 ) {
 			$('#about-us').waypoint( function( direction ) {
-										
+
 				if( direction === 'down' && !$(this).hasClass('animated') ) {
 
 
 					setTimeout(aboutAnimate, 200);
 
-					
+
 					$(this.element).addClass('animated');
-						
+
 				}
 			} , { offset: '95%' } );
 		}
@@ -215,15 +218,15 @@
 	// Team
 	var teamAnimate = function() {
 
-		if ( $('#team').length > 0 ) {	
+		if ( $('#team').length > 0 ) {
 			$('#team .to-animate').each(function( k ) {
-				
+
 				var el = $(this);
-				
+
 				setTimeout ( function () {
 					el.addClass('fadeInUp animated');
 				},  k * 200, 'easeInOutExpo' );
-				
+
 			});
 		}
 
@@ -232,15 +235,15 @@
 
 		if ( $('#team').length > 0 ) {
 			$('#team').waypoint( function( direction ) {
-										
+
 				if( direction === 'down' && !$(this).hasClass('animated') ) {
 
 
 					setTimeout(teamAnimate, 200);
 
-					
+
 					$(this.element).addClass('animated');
-						
+
 				}
 			} , { offset: '95%' } );
 		}
@@ -250,15 +253,15 @@
 	// Services
 	var servicesAnimate = function() {
 
-		if ( $('#fh5co-our-services').length > 0 ) {	
+		if ( $('#fh5co-our-services').length > 0 ) {
 			$('#fh5co-our-services .to-animate').each(function( k ) {
-				
+
 				var el = $(this);
-				
+
 				setTimeout ( function () {
 					el.addClass('fadeInUp animated');
 				},  k * 200, 'easeInOutExpo' );
-				
+
 			});
 		}
 
@@ -267,15 +270,15 @@
 
 		if ( $('#fh5co-our-services').length > 0 ) {
 			$('#fh5co-our-services').waypoint( function( direction ) {
-										
+
 				if( direction === 'down' && !$(this).hasClass('animated') ) {
 
 
 					setTimeout(servicesAnimate, 200);
 
-					
+
 					$(this.element).addClass('animated');
-						
+
 				}
 			} , { offset: '95%' } );
 		}
@@ -286,15 +289,15 @@
 	// Features
 	var featuresAnimate = function() {
 
-		if ( $('#fh5co-features').length > 0 ) {	
+		if ( $('#fh5co-features').length > 0 ) {
 			$('#fh5co-features .to-animate').each(function( k ) {
-				
+
 				var el = $(this);
-				
+
 				setTimeout ( function () {
 					el.addClass('fadeInUp animated');
 				},  k * 200, 'easeInOutExpo' );
-				
+
 			});
 		}
 
@@ -303,7 +306,7 @@
 
 		if ( $('#fh5co-features').length > 0 ) {
 			$('#fh5co-features').waypoint( function( direction ) {
-										
+
 				if( direction === 'down' && !$(this).hasClass('animated') ) {
 
 
@@ -318,9 +321,9 @@
 						$('.animate-features-3').addClass('animated fadeInUp');
 					}, 1400);
 
-					
+
 					$(this.element).addClass('animated');
-						
+
 				}
 			} , { offset: '95%' } );
 		}
@@ -331,15 +334,15 @@
 	// testimonials
 	var testimonialsAnimate = function() {
 
-		if ( $('#fh5co-testimonials').length > 0 ) {	
+		if ( $('#fh5co-testimonials').length > 0 ) {
 			$('#fh5co-testimonials .to-animate').each(function( k ) {
-				
+
 				var el = $(this);
-				
+
 				setTimeout ( function () {
 					el.addClass('fadeInUp animated');
 				},  k * 200, 'easeInOutExpo' );
-				
+
 			});
 		}
 
@@ -348,14 +351,14 @@
 
 		if ( $('#fh5co-testimonials').length > 0 ) {
 			$('#fh5co-testimonials').waypoint( function( direction ) {
-										
-				
+
+
 					setTimeout(testimonialsAnimate, 200);
-					
-					
+
+
 					$(this.element).addClass('animated');
-						
-			
+
+
 			} , { offset: '95%' } );
 		}
 
@@ -364,15 +367,15 @@
 	// Pricing
 	var pricingAnimate = function() {
 
-		if ( $('#fh5co-pricing').length > 0 ) {	
+		if ( $('#fh5co-pricing').length > 0 ) {
 			$('#fh5co-pricing .to-animate').each(function( k ) {
-				
+
 				var el = $(this);
-				
+
 				setTimeout ( function () {
 					el.addClass('fadeInUp animated');
 				},  k * 200, 'easeInOutExpo' );
-				
+
 			});
 		}
 
@@ -381,7 +384,7 @@
 
 		if ( $('#fh5co-pricing').length > 0 ) {
 			$('#fh5co-pricing').waypoint( function( direction ) {
-										
+
 					setTimeout(function(){
 						$('.animate-pricing-1').addClass('animated fadeIn');
 					}, 200);
@@ -389,11 +392,11 @@
 						$('.animate-pricing-2').addClass('animated fadeIn');
 					}, 300);
 					setTimeout(pricingAnimate, 700);
-					
-					
+
+
 					$(this.element).addClass('animated');
-						
-			
+
+
 			} , { offset: '95%' } );
 		}
 
@@ -402,15 +405,15 @@
 	// Pricing
 	var pressAnimate = function() {
 
-		if ( $('#fh5co-press').length > 0 ) {	
+		if ( $('#fh5co-press').length > 0 ) {
 			$('#fh5co-press .to-animate').each(function( k ) {
-				
+
 				var el = $(this);
-				
+
 				setTimeout ( function () {
 					el.addClass('fadeInUp animated');
 				},  k * 200, 'easeInOutExpo' );
-				
+
 			});
 		}
 
@@ -419,7 +422,7 @@
 
 		if ( $('#fh5co-press').length > 0 ) {
 			$('#fh5co-press').waypoint( function( direction ) {
-										
+
 					setTimeout(function(){
 						$('.animate-press-1').addClass('animated fadeIn');
 					}, 200);
@@ -427,19 +430,19 @@
 						$('.animate-press-2').addClass('animated fadeIn');
 					}, 300);
 					setTimeout(pressAnimate, 700);
-					
-					
+
+
 					$(this.element).addClass('animated');
-						
-			
+
+
 			} , { offset: '95%' } );
 		}
 
 	};
 
 
-	
-	
+
+
 
 	// Document on load.
 	$(function(){
